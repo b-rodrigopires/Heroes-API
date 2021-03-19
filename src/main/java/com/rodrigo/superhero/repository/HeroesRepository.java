09e0cb23a27a11de167710b53f8cd099ef6fb00d
Rodrigo Pires <b.rodrigopires@gmail.com>
2021-03-19 17:04:36 -0300

    API Rest para gerenciamento de herois

---
+++ src/main/java/com/rodrigo/superhero/repository/HeroesRepository.java
@@ -0,0 +1,9 @@
+package com.rodrigo.superhero.repository;
+
+import com.rodrigo.superhero.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+}
